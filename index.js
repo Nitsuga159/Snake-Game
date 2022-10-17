5cd60e92baeeaf2e41747f6a91eafe7a949e3ae9
Agustin Romero <agusromero0815@gmail.com>
2022-10-17 16:36:05 -0300

    First Commit

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 import App from "./App.js";
 
-document.addEventListener("DOMContentLoaded", () => App(700, 35, 35));
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener(
+    "keydown",
+    function (e) {
+      if (
+        ["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(
+          e.code
+        ) > -1
+      ) {
+        e.preventDefault();
+      }
+    },
+    false
+  );
+
+  App(700, 35, 35);
+});
